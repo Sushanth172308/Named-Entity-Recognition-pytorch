d66607529a28f157a74908c06ec3589ed1e37b29
SUSHANTH <sushanth172308@gmail.com>
2022-07-03 11:01:04 +0100

    setup file updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="ner",
     version="0.0.1",
-    author="Ketan Gangal",
-    author_email="ketangangal98@gmail.com",
+    author="Sushanth172308",
+    author_email="sushanth172308@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
